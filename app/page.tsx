908fef203416a38e67c0065410a043499c01b0c7
Pierre Guillemot <pierregllmt@gmail.com>
2024-02-15 18:13:21 +0100

    implementing hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,29 @@
 "use client";
 
+import PageContainer from "@/components/page-container";
 import { Button } from "@/components/ui/button";
-import { Eye } from "lucide-react";
-import Image from "next/image";
-import { useRouter } from "next/navigation";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
-  const router = useRouter();
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button 
-        variant="outline"
-        onClick={() => router.push('/login')}
-      >
-        <Eye size={24} /> 
-        Login
-      </Button>
-      <Button 
-        variant="outline"
-        onClick={() => router.push('/categories/react')}
-      >
-        <Eye size={24} /> 
-        Page React
-      </Button>
-    </main>
+    <PageContainer>
+      <div className="py-10 px-4">
+        <div
+          style={{backgroundImage: "url('/img/hero_bg.jpg')"}}
+          className="rounded-lg aspect-square md:aspect-[2.4/1] overflow-hidden bg-cover bg-center shadow-lg"
+        >
+          <div className="w-full h-full flex flex-col justify-center items-center">
+              <div className="sm:max-w-xl max-w-xs bg-secondary-foreground/80 p-4 rounded-lg">
+                <h1 className="font-bold text-3xl sm:text-5xl text-white dark:text-black text-center">
+                  Become A Better React Developer
+                </h1>
+                <Input type="email" placeholder="Enter your email" className="light:bg-white mt-4" />
+                <Button size="lg" className="w-full py-6 text-xl mt-4 text-white dark:text-black light:bg-white">Subscibe to our newsletter</Button>
+              </div>
+          </div>
+        </div>
+      </div>
+    </PageContainer>
   );
 }
--- utils/types.ts
+++ utils/types.ts
@@ -3,3 +3,17 @@ export type Category = {
     name: string;
     slug: string;
 }
+export type Post = {
+    id: number;
+    category: string;
+    title: string;
+    image: string;
+    caption: string;
+    date: string | Date;
+    minutesToRead: number;
+    author: string;
+    nbViews: number;
+    nbComments: number;
+    slug: string;
+    content?: string;
+  };
